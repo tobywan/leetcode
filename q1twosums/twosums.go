d6ad1033269082bd23eefc02cdbcd0a144e6f9b6
tobywan <wxyz4321@hotmail.com>
2018-05-24 18:19:24 +0100

    Q1. Two Sum

---
+++ q1twosums/twosums.go
@@ -0,0 +1,30 @@
+package q1twosum
+
+func twoSum(nums []int, target int) []int {
+	// read into map with index:
+	idx := make(map[int][]int, len(nums))
+
+	for i, n := range nums {
+		idx[n] = append(idx[n], i)
+	}
+
+	for i, n := range nums {
+		diff := target - n
+
+		if ixs, ok := idx[diff]; ok {
+			if len(ixs) > 1 {
+				// we've been told only one answer, so
+				// diff must == n
+				return ixs[:2]
+			}
+			j := ixs[0]
+			if i == j {
+				continue //ignore same index
+			}
+			return []int{i, j}
+
+		}
+
+	}
+	return []int{-1, -1}
+}
---
+++ q1twosums/twosums_test.go
@@ -0,0 +1,52 @@
+package q1twosum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAnswer(t *testing.T) {
+	tests := []struct {
+		in1  []int
+		in2  int
+		want []int
+	}{
+		{
+			in1:  []int{1, 2, 4, 8, 16, 32, 64, 128},
+			in2:  192,
+			want: []int{6, 7},
+		},
+		{
+			in1:  []int{1, 2, 4, 8, 16, 32, 64, 128},
+			in2:  10,
+			want: []int{1, 3},
+		},
+		{
+			in1:  []int{1, 2, 4, 8, 16, 32, 64, 128},
+			in2:  129,
+			want: []int{0, 7},
+		},
+		{
+			in1:  []int{2, 7, 11, 1},
+			in2:  9,
+			want: []int{0, 1},
+		},
+		{
+			in1:  []int{3, 2, 4},
+			in2:  6,
+			want: []int{1, 2},
+		},
+		{
+			in1:  []int{3, 3},
+			in2:  6,
+			want: []int{0, 1},
+		},
+	}
+	for _, test := range tests {
+		got := twoSum(test.in1, test.in2)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("twoSum(%v,%d)=%v, want%v", test.in1, test.in2, got, test.want)
+		}
+	}
+
+}
