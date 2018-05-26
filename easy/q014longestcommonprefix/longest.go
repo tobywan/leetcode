73e4802bea4de8dff2661433511e864abcbdd0fc
tobywan <wxyz4321@hotmail.com>
2018-05-26 07:26:08 +0100

    Your runtime beats 100.00 % of golang submissions.

--- easy/q014longestcommonprefix/longest.go
+++ easy/q014longestcommonprefix/longest.go
@@ -1,38 +1,5 @@
 package longestcommonprefix
 
-// // longestCommonPrefix looks at successive initials
-// // to see which are in common
-// func longestCommonPrefix(strs []string) string {
-// 	if len(strs) == 0 {
-// 		return ""
-// 	}
-// 	if len(strs) == 1 {
-// 		return strs[0]
-// 	}
-// 	var b bytes.Buffer
-// 	pos := 0
-// 	cont := true
-
-// 	for cont && pos < len(strs[0]{
-// 		r := strings. (strs[0], pos)
-// 		for _, s := range strs {
-// 			if len(s) >= pos || r != s[pos]{
-// 				cont = false
-// 				break
-// 			}
-// 		}
-// 		if cont {
-// 			b.WriteRune(r)
-// 			pos ++
-// 		}
-// 	}
-
-// 	return b.String()
-// }
-
-// longestCommonPrefix stores a prefix as runes and compares
-// each string against them
-
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
@@ -40,79 +7,38 @@ func longestCommonPrefix(strs []string) string {
 	if len(strs) == 1 {
 		return strs[0]
 	}
-	pref := []rune(strs[0])
+	pref := strs[0][:]
 
 	for _, s := range strs[1:] {
 		pref = commonPrefix(pref, s)
 	}
 
-	return string(pref)
+	return pref
 }
 
-// commonPrefix finds the prefix runes p has in common with s
-func commonPrefix(p []rune, s string) []rune {
-	l := len(p)
-	if l == 0 {
-		return p
+// commonPrefix finds the prefix p has in common with s
+func commonPrefix(p string, s string) string {
+	lp := len(p)
+	ls := len(s)
+	if lp == 0 || ls == 0 {
+		return ""
 	}
 
-	common := 0
-	for n, r := range s {
-		if n >= l || p[n] != r {
+	var lc int // length of shorter string
+	if lp < ls {
+		lc = lp
+	} else {
+		lc = ls
+	}
+
+	var cb int // common bytes
+
+	for cb = 0; cb < lc; cb++ {
+		if p[cb] != s[cb] {
 			break
 		}
-		common++
 	}
-	return p[:common]
+
+	return p[:cb]
 
 }
-
-// func longestCommonPrefix(strs []string) string {
-
-// 	l := &lexTree{}
-
-// 	for _, s := range strs {
-// 		l.append([]rune(s))
-// 	}
-// 	return l.commonPrefix()
-
-// }
-
-// type lexTree struct {
-// 	kids map[rune](*lexTree)
-// }
-
-// // append looks to see if s can be added to any kids
-// func (l *lexTree) append(s []rune) {
-// 	if len(s) == 0 {
-// 		return
-// 	}
-
-// 	if l.kids == nil {
-// 		l.kids = make(map[rune]*lexTree)
-// 	}
-// 	var lt lexTree
-// 	if lt, ok := l.kids[s[0]]; !ok {
-// 		// already have an entry for initial letter
-// 		lt = &lexTree{}
-// 		l.kids[s[0]] = lt
-// 	}
-// 	lt.append(s[1:])
-// }
-
-// func (l *lexTree) commonPrefix() string {
-// 	var b bytes.Buffer
-
-// 	x := len(l.kids)
-// 	kid := l
-// 	for x == 1 {
-// 		for k, v := range kid.kids {
-// 			b.WriteRune(k)
-// 			kid = v
-// 		}
-// 		x = len(kid.kids)
-
-// 	}
-
-// 	return b.String()
-// }
--- easy/q014longestcommonprefix/longest_test.go
+++ easy/q014longestcommonprefix/longest_test.go
@@ -2,8 +2,18 @@ package longestcommonprefix
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
+func TestStringSlice(t *testing.T) {
+	s := "\u1234\u2222"
+
+	for i := 0; i <= len(s); i++ {
+		tmp := s[:i]
+		t.Logf("s[:%d] = %q, is Valid %t", i, tmp, utf8.ValidString(tmp))
+	}
+
+}
 func BenchmarkLongestCommonPrefix(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
@@ -15,6 +25,14 @@ func TestLongestCommonPrefix(t *testing.T) {
 		in   []string
 		want string
 	}{
+		{
+			in:   []string{"\u1234flower", "\u1234flow", "\u1234flight"},
+			want: "\u1234fl",
+		},
+		{
+			in:   []string{"fl\u2222ower", "fl\u2222ow", "fl\u2222ight"},
+			want: "fl\u2222",
+		},
 		{
 			in:   []string{"flower", "flow", "flight"},
 			want: "fl",
