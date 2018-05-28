59b62d631883dee0993233597d854c0c4996a765
tobywan <wxyz4321@hotmail.com>
2018-05-28 22:07:18 +0100

    Add Q101

---
+++ easy/q101symmetrictree/symmetrictree.go
@@ -0,0 +1,21 @@
+package symmetrictree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return false
+
+}
+
+// TreeNode is node of a tree
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
