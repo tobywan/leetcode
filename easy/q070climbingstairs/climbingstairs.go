79e53f187321a6d96f89c5925c79d3c55926100d
tobywan <wxyz4321@hotmail.com>
2018-05-28 21:45:15 +0100

    Q70 Climbing Stairs

--- easy/q070climbingstairs/climbingstairs.go
+++ easy/q070climbingstairs/climbingstairs.go
@@ -1,5 +1,38 @@
 package climbingstairs
 
 func climbStairs(n int) int {
-	return 0
+
+	cache := make(intMap)
+
+	return climbStair(n, cache)
+
+}
+
+type intMap map[int]int
+
+func climbStair(n int, cache intMap) int {
+
+	if n < 1 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	if n == 2 {
+		return 2
+	}
+
+	ret, ok := cache[n]
+
+	if !ok {
+		ret = climbStair(n-1, cache) + climbStair(n-2, cache)
+		cache[n] = ret
+	}
+	// } else {
+	// 	fmt.Printf("Cache hit for %d\n", n)
+	// }
+
+	return ret
 }
--- easy/q070climbingstairs/climbingstairs_test.go
+++ easy/q070climbingstairs/climbingstairs_test.go
@@ -1,1 +1,49 @@
 package climbingstairs
+
+import (
+	"testing"
+)
+
+func TestClimbStairs(t *testing.T) {
+	tests := []struct {
+		in   int
+		want int
+	}{
+		{
+			in:   1,
+			want: 1,
+		},
+		{
+			in:   2,
+			want: 2,
+		},
+		{
+			in:   3,
+			want: 3,
+		},
+		{
+			in:   4,
+			want: 5,
+		},
+		{
+			in:   5,
+			want: 8,
+		},
+		{
+			in:   6,
+			want: 13,
+		},
+		{
+			in:   7,
+			want: 21,
+		},
+	}
+
+	for _, test := range tests {
+		got := climbStairs(test.in)
+		if got != test.want {
+			t.Errorf("climbStairs(%d) = %d, want %d", test.in, got, test.want)
+		}
+	}
+
+}
