92a36589ef87c577e56e46967f5752d12bdda88f
tobywan <wxyz4321@hotmail.com>
2018-05-29 20:51:05 +0100

    Add Q101

---
+++ easy/q101maxdepthbintree/maxdepth.go
@@ -0,0 +1,20 @@
+package maxdepthbintree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	return 0
+}
+
+// TreeNode is A definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
