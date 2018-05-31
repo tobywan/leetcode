1eb44a1dde63093b7bb74490e7c013e8771ff7ca
tobywan <wxyz4321@hotmail.com>
2018-05-31 21:45:02 +0100

    Add Q111

---
+++ easy/q111mindepthbintree/mindepth.go
@@ -0,0 +1,12 @@
+package mindepthbintree
+
+// TreeNode is A definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	return 0
+}
