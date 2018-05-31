29361068b1e2019795f10ac623d20f5039b142f5
tobywan <wxyz4321@hotmail.com>
2018-05-31 07:34:07 +0100

    Add Q107

---
+++ easy/q107binarytreetraverse2/levelorderbottom.go
@@ -0,0 +1,13 @@
+package binarytreetraverse2
+
+// TreeNode is a definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+
+	return [][]int{}
+}
