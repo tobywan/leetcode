ab55317c3dadb6006a4c572faa0cec7239d7eaa2
tobywan <wxyz4321@hotmail.com>
2018-06-01 07:55:05 +0100

    Add Q112

---
+++ easy/q112pathsum/pathsum.go
@@ -0,0 +1,12 @@
+package pathsum
+
+// TreeNode is a definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	return false
+}
