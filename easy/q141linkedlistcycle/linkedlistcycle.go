2a6cf2a7f20306a92460e7a6af82c77267c67bc3
tobywan <wxyz4321@hotmail.com>
2018-06-05 12:34:25 +0100

    Add Q141

---
+++ easy/q141linkedlistcycle/linkedlistcycle.go
@@ -0,0 +1,14 @@
+package linkedlistcycle
+
+func hasCycle(head *ListNode) bool {
+	return false
+}
+
+////////////////////////
+// local code only for test and display
+
+// ListNode is a Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
