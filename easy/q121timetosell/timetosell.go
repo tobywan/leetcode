c590b2368963a93e18d2a310696272f15835a36b
tobywan <wxyz4321@hotmail.com>
2018-06-05 11:24:00 +0100

    Your runtime beats 100.00 % of golang submissions.

--- easy/q121timetosell/timetosell.go
+++ easy/q121timetosell/timetosell.go
@@ -10,6 +10,28 @@ func maxProfit(prices []int) int {
 	buy := prices[0]
 
 	for _, n := range prices[1:] {
+		if n == buy {
+			continue
+		}
+		if n > buy {
+			profit = max(profit, n-buy)
+		} else {
+			buy = n
+		}
+	}
+	return profit
+}
+
+func maxProfit1(prices []int) int {
+
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	profit := 0
+	buy := prices[0]
+
+	for _, n := range prices[1:] {
 		switch {
 
 		case n > buy:
--- easy/q121timetosell/timetosell_test.go
+++ easy/q121timetosell/timetosell_test.go
@@ -4,6 +4,18 @@ import (
 	"testing"
 )
 
+func BenchmarkMaxProfit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		maxProfit([]int{1, 4, 65, 8, 90, 45, 3, 234, 5, 7, 8, 9, 8, 6, 4, 3, 3, 455, 7, 8, 8, 5, 456, 74, 78, 34, 6, 7, 8, 8, 65, 5, 34, 3, 3, 4, 5, 677, 8, 99, 7, 65, 4})
+	}
+}
+
+func BenchmarkMaxProfit1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		maxProfit1([]int{1, 4, 65, 8, 90, 45, 3, 234, 5, 7, 8, 9, 8, 6, 4, 3, 3, 455, 7, 8, 8, 5, 456, 74, 78, 34, 6, 7, 8, 8, 65, 5, 34, 3, 3, 4, 5, 677, 8, 99, 7, 65, 4})
+	}
+}
+
 func TestMaxProfit(t *testing.T) {
 	tests := []struct {
 		in   []int
