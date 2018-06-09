25555f2c3dbaf43da376a1706ffcac34f2880573
tobywan <wxyz4321@hotmail.com>
2018-06-09 14:30:32 +0100

    Q122 Attempt to improve

--- easy/q122timetosell2/timetosell.go
+++ easy/q122timetosell2/timetosell.go
@@ -12,6 +12,35 @@ func maxProfit(prices []int) int {
 
 	for i, this := range prices[1:] {
 		last := prices[i]
+
+		if last <= this {
+			// a profit to be made
+			subProfit = this - buy
+		} else {
+			// prices have gone down since yesterday
+			buy = this
+			totalProfit += subProfit
+			subProfit = 0
+		}
+	}
+	totalProfit += subProfit
+
+	return totalProfit
+
+}
+
+func maxProfit1(prices []int) int {
+
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	totalProfit := 0
+	subProfit := 0
+	buy := prices[0]
+
+	for i, this := range prices[1:] {
+		last := prices[i]
 		if last == this {
 			// no change in price
 			continue
--- easy/q122timetosell2/timetosell_test.go
+++ easy/q122timetosell2/timetosell_test.go
@@ -2,6 +2,22 @@ package timetosell2
 
 import "testing"
 
+func BenchmarkMaxProfit(b *testing.B) {
+	in := []int{1, 2, 3, 5, 7, 54, 3, 2, 1, 3, 5, 6, 9, 0, 9, 7, 5, 34, 23, 3, 4, 5, 67, 8, 99, 75, 4, 3, 23}
+	for i := 0; i < b.N; i++ {
+		maxProfit(in)
+	}
+
+}
+
+func BenchmarkMaxProfit1(b *testing.B) {
+	in := []int{1, 2, 3, 5, 7, 54, 3, 2, 1, 3, 5, 6, 9, 0, 9, 7, 5, 34, 23, 3, 4, 5, 67, 8, 99, 75, 4, 3, 23}
+	for i := 0; i < b.N; i++ {
+		maxProfit1(in)
+	}
+
+}
+
 func TestMaxProfit(t *testing.T) {
 	tests := []struct {
 		in   []int
