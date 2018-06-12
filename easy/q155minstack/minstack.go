71e85cb0b184fde663df5a964c4bdb0b5787e4c9
tobywan <wxyz4321@hotmail.com>
2018-06-12 20:45:37 +0100

    Your runtime beats 100.00 % of golang submissions.

--- easy/q155minstack/minstack.go
+++ easy/q155minstack/minstack.go
@@ -1,57 +1,55 @@
 package minstack
 
 // MinStack is a stack thst keeps track of the minimum
-// TODO what is the current min after popping
 type MinStack struct {
-	stack []int
-	mins  []int
+	stack []element
+}
+
+type element struct {
+	value int
+	min   int
 }
 
 // Constructor creates a new MinStack
 func Constructor() MinStack {
-	return MinStack{[]int{}, []int{}}
+	return MinStack{[]element{}}
 }
 
 // Push adds an item to the top of the stack
 func (m *MinStack) Push(x int) {
-	m.stack = append(m.stack, x)
 
-	l := len(m.mins)
-
-	if l == 0 {
-		m.mins = append(m.mins, x)
-		return
+	min := x
+	l := len(m.stack)
+	if l > 0 {
+		min = m.stack[l-1].min
+		if x < min {
+			min = x
+		}
 	}
 
-	min := m.mins[l-1]
-
-	if x < min {
-		min = x
-	}
-	m.mins = append(m.mins, min)
+	m.stack = append(m.stack, element{x, min})
 
 }
 
 // Pop removes the top element
 func (m *MinStack) Pop() {
 	m.stack = m.stack[:len(m.stack)-1]
-	m.mins = m.mins[:len(m.mins)-1]
 }
 
-// Top reports the top element withoug changing the stack
+// Top reports the top element without changing the stack
 func (m *MinStack) Top() int {
-	return last(m.stack)
+	return last(m.stack).value
 }
 
-func last(s []int) int {
+func last(s []element) element {
 	l := len(s)
 	if l == 0 {
-		return 0
+		return element{0, 0}
 	}
 	return s[l-1]
 }
 
 // GetMin returns the current minimum value
 func (m *MinStack) GetMin() int {
-	return last(m.mins)
+	return last(m.stack).min
 }
