93e79abebb7368fe60b86a9edf2f15b28bc49d3d
tobywan <17815047+tobywan@users.noreply.github.com>
2024-03-29 10:07:21 +0000

    88. Merge Sorted Array

--- csharp/LeetCode150/q088_Merge_Sorted_Array/Program.cs
+++ csharp/LeetCode150/q088_Merge_Sorted_Array/Program.cs
@@ -1,13 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
-public class Solution
+namespace q088
 {
-    public static void Main()
-    {
-    }
-    public void Merge(int[] nums1, int m, int[] nums2, int n)
-    {
 
+    public class Solution
+    {
+        public static void Main()
+        {
+        }
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int mIdx = m - 1;
+            int nIdx = n - 1;
+            int resultIdx = m + n - 1;
+
+            while (resultIdx >= 0)
+            {
+                bool pickM = (mIdx >= 0 && (nIdx < 0 || nums2[nIdx] < nums1[mIdx]));
+
+                if (pickM)
+                {
+                    nums1[resultIdx] = nums1[mIdx];
+                    mIdx--;
+                    resultIdx--;
+                }
+                else
+                {
+                    nums1[resultIdx] = nums2[nIdx];
+                    nIdx--;
+                    resultIdx--;
+                }
+            }
+        }
     }
 }
